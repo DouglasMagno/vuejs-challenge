1e0bac425dc220fe9d34785c8cd2f1fa9e4e27f6
@douglas.magno <douglas.magno@in8.com.br>
2020-10-24 12:29:07 -0300

    style: first changes main app layout

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,16 @@ export default {
 </script>
 
 <style>
+
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 660px;
+  margin: 0 auto;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +35,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+
 </style>
