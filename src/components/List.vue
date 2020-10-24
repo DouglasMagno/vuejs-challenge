216985b7c3028862499eff4055f9982fa1f63195
@douglas.magno <douglas.magno@in8.com.br>
2020-10-24 12:37:14 -0300

    feat: create input to add a new list

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,41 @@
 <template>
-
+  <div>
+    <input type="text" v-model="newList" class="todo-input" placeholder="Add a new list and press enter"
+           @keyup.enter="addList">
+  </div>
 </template>
 
 <script>
 export default {
-name: "List"
+  name: "List",
+  data() {
+    return {
+      newList: '',
+      lists: [
+        {
+          id: "First List",
+          cards: [],
+        }
+      ],
+    };
+  },
+  methods: {
+    addList() {
+      if (this.newList.trim().length === ) {
+        return;
+      }
+
+      this.$set(
+          this.lists,
+          this.lists.length,
+          {
+            id: this.newList,
+            cards: [],
+          }
+      );
+      this.newList = '';
+    }
+  }
 }
 </script>
 
