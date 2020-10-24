9ba923d33b9c85e129c66275b489033aa7faf571
@douglas.magno <douglas.magno@in8.com.br>
2020-10-24 12:59:31 -0300

    feat: add first draggable area with styles and methods

--- src/components/List.vue
+++ src/components/List.vue
@@ -10,6 +10,15 @@
       @drag-start="dragStart"
       class="wrapper-two-column"
     >
+      <Draggable v-for="(list, indexList) in lists" :key="list.id">
+        <div class="card-container">
+          <div class="card-column-header">
+            <span title="Add todo" class="add-item" @click="addTodo(list.id)">&plus;</span>
+            <span class="column-drag-handle">&#x2630;</span>
+            {{ list.id }} <span title="Remove list" class="remove-item" @click="removeList(list.id)">&times;</span>
+          </div>
+        </div>
+      </Draggable>
     </Container>
 
   </div>
@@ -35,6 +44,9 @@ export default {
     };
   },
   methods: {
+    /**
+     * Add list to lists array if has string length
+     */
     addList() {
       if (this.newList.trim().length === ) {
         return;
@@ -50,6 +62,33 @@ export default {
       );
       this.newList = '';
     },
+
+    /**
+     * Remove list from a list id
+     * @param list
+     */
+    removeList(list) {
+      this.$delete(this.lists, this.getListKey(list));
+    },
+
+    /**
+     * Add todocard method
+     * @param list
+     */
+    addTodo(list) {
+      const key = this.getListKey(list);
+      this.$set(
+          this.lists[key].cards,
+          this.lists[key].cards.length,
+          {
+            name: "",
+            id: (this.lists[key].cards.length + 1),
+            editing: true
+          }
+      );
+
+    },
+
     /**
      * Event handler when drop a column, perform and change positions on list array
      * @param dropResult
@@ -63,6 +102,15 @@ export default {
      */
     dragStart() {
 
+    },
+
+    /**
+     * Get list key from a list id
+     * @param listId
+    */
+    getListKey(listId) {
+      const column = this.lists.filter(p => p.id === listId)[0];
+      return this.lists.indexOf(column);
     },
 
     /**
@@ -117,4 +165,41 @@ export default {
     grid-template-columns: 48% 49%;
     grid-gap: 10px;
   }
+
+  .card-container {
+    width: 320px;
+    margin: 5px;
+    margin-right: 45px;
+    background-color: #f3f3f3
+  }
+
+  .card, .card-container {
+    padding: 10px;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, .12), 0 1px 1px rgba(0, 0, 0, .24)
+  }
+
+  .card-column-header {
+    font-size: 18px
+  }
+
+  .remove-item {
+    cursor: pointer;
+    text-align: right;
+    float: right;
+
+    &:hover {
+      color: black;
+    }
+  }
+
+  .add-item {
+    cursor: pointer;
+    text-align: right;
+    float: left;
+
+    &:hover {
+      color: black;
+    }
+  }
+
 </style>
